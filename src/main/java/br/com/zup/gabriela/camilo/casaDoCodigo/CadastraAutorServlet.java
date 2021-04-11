b220b4371d0becc9126f31432f5dcdfd916a741c
Gabriela Carvalho Camilo <gabccamilo.dev@gmail.com>
2021-04-10 23:40:57 -0300

    Criando a lógica do método POST em CadastraAutorServlet

--- src/main/java/br/com/zup/gabriela/camilo/casaDoCodigo/CadastraAutorServlet.java
+++ src/main/java/br/com/zup/gabriela/camilo/casaDoCodigo/CadastraAutorServlet.java
@@ -5,17 +5,23 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 @WebServlet(name = "CadastraAutorServlet", value = "/cadastraAutor")
 public class CadastraAutorServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
-        System.out.println("cadastrado");
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1> Cadastrado com sucesso </h1>");
-        out.println("</body></html>");
+        String nomeAutor = request.getParameter("nome");
+        String emailAutor = request.getParameter("email");
+
+        Autor autor = new Autor();
+        autor.setNome(nomeAutor);
+        autor.setEmail(emailAutor);
+
+        Banco banco = new Banco();
+        banco.adiciona(autor);
+
+        response.sendRedirect("listaAutores");
     }
 }
