6a689f7133aa2a2298fcd319b4eb43f4e0984c8b
Gabriela Carvalho Camilo <gabccamilo.dev@gmail.com>
2021-04-10 23:39:01 -0300

    Adicionando métodos à classe Banco

--- src/main/java/br/com/zup/gabriela/camilo/casaDoCodigo/Banco.java
+++ src/main/java/br/com/zup/gabriela/camilo/casaDoCodigo/Banco.java
@@ -1,4 +1,18 @@
 package br.com.zup.gabriela.camilo.casaDoCodigo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Banco {
+    private static List<Autor> listaAutor = new ArrayList<>();
+    private static Integer chaveSequencialAutor = 1;
+
+    public static List<Autor> getListaAutor() {
+        return listaAutor;
+    }
+
+    public void adiciona(Autor empresa) {
+        empresa.setId(Banco.chaveSequencialAutor++);
+        Banco.listaAutor.add(empresa);
+    }
 }
